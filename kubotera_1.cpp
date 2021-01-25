53decccaa938703d2fe4e671d777fd05510e2a1b
Masato Kubotera <s19c1040hd@s.chibakoudai.jp>
2021-01-25 16:06:53 +0900

    make FizzBuzz

---
+++ kubotera_1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+class FizzBuzz{
+    public:
+        void set(int num);
+        void check();
+
+    private:
+        int c_num;
+};
+
+void FizzBuzz::set(int num){
+    c_num = num;
+    check();
+}
+
+void FizzBuzz::check(){
+    std::string str;
+
+    if(c_num % 3 == 0)
+        str += "Fizz";
+
+    if(c_num % 5 == 0)
+        str += "Buzz";
+
+    if( str[0] =='\0')
+        str += "Error";
+
+    std::cout << str << std::endl;
+} 
+
+int main(){
+    FizzBuzz obj;
+    int num;
+    
+    std::cin >> num;
+    obj.set(num);
+
+    return 0;
+}
